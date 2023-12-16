e3bfa7f888e26f5c393f77d79265923e22e19318
AManWithAHart <109753911+AManWithAHart@users.noreply.github.com>
2023-12-16 15:59:59 -0600

    Added Game Start Text

---
+++ csci5611FinalProject/csci5611 Horror Game/IntroText.gd
@@ -0,0 +1,19 @@
+extends TextEdit
+
+
+const max_timer = 3
+var cur_timer = 0
+var displaytext = true
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	if(displaytext):
+		if cur_timer >= max_timer:
+			queue_free()
+		else:
+			cur_timer += delta
